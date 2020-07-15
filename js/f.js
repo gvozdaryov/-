63be6b633b00b53bc583ec36864fa13fe4c494a8
Vlad <gvozdarevv9@gmail.com>
2020-07-15 13:33:34 +0800

    f

---
+++ js/f.js
@@ -0,0 +1,25 @@
+
+
+function f(a) {if (a >0) alert ('!'); else alert('!!')}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function kvadrat(num) {return Math.pow(num,2)}
+function kub(num) {return Math.pow(num,3)}
+
+function ggg(num) {return kvadrat(num) + kub(num)}
+
+alert (ggg(3))
---
+++ js/jss.js
@@ -0,0 +1,3 @@
+let b = 0
+for (let i = 1; i < 101; i++) {b += i}
+console.log(b)
